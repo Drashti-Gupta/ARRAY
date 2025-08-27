d7e29ee4be6ff6d781e7a2bf7cd6ca344bfb66c3
Drashti-Gupta <guptadrashti61@gmail.com>
2025-08-27 15:57:49 +0530

    some arrays files.

---
+++ Marks.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Marks {
+    public static void main(String[] args) {
+        int marks[]={15,20,25};
+        for( int i=0;i<33;i++){
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
